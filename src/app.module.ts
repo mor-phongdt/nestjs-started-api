a1449557c5b288bf1185ab6dfa3d307d26e9ba99
cadang1303 <dangnguyen130399@gmail.com>
2023-12-20 09:43:48 +0700

    [be] update logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/users/user.module';
 import { ChallengeModule } from './modules/challenge/challenge.module';
 import { PrismaModule } from './database/prisma.module';
+import LogsMiddleware from './utils/logs.middleware';
 
 @Module({
   imports: [
@@ -18,4 +19,8 @@ import { PrismaModule } from './database/prisma.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LogsMiddleware).forRoutes('*');
+  }
+}
