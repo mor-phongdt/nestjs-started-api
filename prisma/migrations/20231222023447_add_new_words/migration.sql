deae0d57a5228d7eb37050b0675f6d5906990a49
TuND <ductu1411@gmail.com>
2023-12-26 14:28:43 +0700

    [back] Update table newWords

--- prisma/migrations/20231222023447_add_new_words/migration.sql
+++ prisma/migrations/20231222023447_add_new_words/migration.sql
@@ -2,10 +2,13 @@
 CREATE TABLE "NewWords" (
     "id" SERIAL NOT NULL,
     "word" TEXT NOT NULL,
-    "mean" TEXT NOT NULL,
+    "definition" JSONB NOT NULL,
     "example" TEXT NOT NULL,
     "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "NewWords_pkey" PRIMARY KEY ("id")
 );
+
+CREATE UNIQUE INDEX "NewWords_word_key" ON "NewWords"("word");
+
--- src/modules/words/dto/word.dto.ts
+++ src/modules/words/dto/word.dto.ts
@@ -1,14 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
+class DefinitionJsonDto {
+  @ApiProperty({
+    example: 'typeValue',
+    description: 'Type property value',
+  })
+  @IsString()
+  type: string;
+
+  @ApiProperty({
+    example: 'meaningValue',
+    description: 'Meaning property value',
+  })
+  @IsString()
+  meaning: string;
+}
 export class NewWordsDto {
   @ApiProperty()
   @IsString()
   word: string;
 
   @ApiProperty()
-  @IsString()
-  mean: string;
+  @IsNotEmpty()
+  definition: DefinitionJsonDto;
 
   @ApiProperty()
   @IsString()
